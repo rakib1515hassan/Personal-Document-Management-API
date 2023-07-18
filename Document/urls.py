dfc2fbf42917355576e8100f134e8face445cd70
Md Rakib Hassan <rakib1515hassan@gmail.comm>
2023-07-19 00:39:03 +0600

    User and Admin Create, Update, Delete Documents

--- Document/urls.py
+++ Document/urls.py
@@ -5,5 +5,12 @@ from Document import views
 
 
 urlpatterns = [
-    path('document-create/', views.DocumentsCreateAPIView.as_view()),
+    path('document-create/', views.UserDocumentsListCreateAPIView.as_view()),
+
+    path('document-retrieve-update-delete/<int:pk>/', views.UserDocumentsRetrieveUpdateDestroyAPIView.as_view()),
+
+    path('admin/document-retrieve/', views.AdminDocumentsListAPIView.as_view()),
+
+    path('admin/document-retrieve-update-delete/<int:pk>/', views.AdminDocumentsRetrieveUpdateDestroyAPIView.as_view()),
+
 ]
--- Document/views.py
+++ Document/views.py
@@ -4,19 +4,61 @@ from Account.models import Documents
 from Document.serializers import DocumentsSerializers
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.db.models import Q
 
 
 # Create your views here.
 # URL = ( http://127.0.0.1:8000/document/document-create/ )
-class DocumentsCreateAPIView(generics.CreateAPIView):
+class UserDocumentsListCreateAPIView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = DocumentsSerializers
 
+    def get_queryset(self):
+        user = self.request.user  # Get the current user
+        return Documents.objects.filter(user=user)
 
-# class DocumentsRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-#     permission_classes = [IsAuthenticated]
-#     serializer_class = DocumentsSerializers
 
-#     def get_queryset(self):
-#         user_id = self.kwargs['pk']
-#         return Documents.objects.filter()
+
+
+# URL = ( http://127.0.0.1:8000/document/document-retrieve-update-delete/id/ )
+class UserDocumentsRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = DocumentsSerializers
+
+    def get_queryset(self):
+        user = self.request.user  # Get the current user
+        return Documents.objects.filter(user=user)
+    
+
+
+
+
+
+# URL = ( http://127.0.0.1:8000/document/admin/document-retrieve/ )
+class AdminDocumentsListAPIView(generics.ListAPIView):
+    queryset = Documents.objects.all()
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    serializer_class = DocumentsSerializers
+
+
+
+
+
+# URL = ( http://127.0.0.1:8000/document/admin/document-retrieve-update-delete/id/ )
+class AdminDocumentsRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    serializer_class = DocumentsSerializers
+
+    def get_queryset(self):
+        user = self.request.user  # Get the current user
+        if user.is_superuser == True:
+            doc_id = self.kwargs['pk']
+            return Documents.objects.filter(pk = doc_id)
+
+
+
